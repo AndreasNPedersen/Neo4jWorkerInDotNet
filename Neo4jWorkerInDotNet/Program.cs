0000709155e56161479a8f36131f478289ee4c9a
Andreas Nørregaard Pedersen <anpe53920@edu.ucl.dk>
2023-11-08 10:14:55 +0100

    Add project files.

---
+++ Neo4jWorkerInDotNet/Program.cs
@@ -0,0 +1,17 @@
+namespace Neo4jWorkerInDotNet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
